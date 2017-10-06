d89fea72c45fb9e849e9e96bf99afd7a96825312
vle28 <vle28@csc.com>
2017-10-06 21:32:31 +0700

    Create new web application project

---
+++ kawaiizumi-web-app/src/main/java/com/izumi/product/kawaiizumiwebapp/KawaiizumiWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.izumi.product.kawaiizumiwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KawaiizumiWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KawaiizumiWebAppApplication.class, args);
+	}
+}
---
+++ kawaiizumi-web-app/src/test/java/com/izumi/product/kawaiizumiwebapp/KawaiizumiWebAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.izumi.product.kawaiizumiwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KawaiizumiWebAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
