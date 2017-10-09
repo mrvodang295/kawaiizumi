9f87c8d607ac5af41e29e2a7b76898b218504d1a
vle28 <vle28@csc.com>
2017-10-09 10:28:33 +0700

    Create new project for management of chicken.

---
+++ msg-chicken-management/src/main/java/com/kawaiizumi/services/msgchickenmanagement/MsgChickenManagementApplication.java
@@ -0,0 +1,12 @@
+package com.kawaiizumi.services.msgchickenmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgChickenManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgChickenManagementApplication.class, args);
+	}
+}
---
+++ msg-chicken-management/src/test/java/com/kawaiizumi/services/msgchickenmanagement/MsgChickenManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kawaiizumi.services.msgchickenmanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsgChickenManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
