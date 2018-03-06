7e5650b8bbbb52316c82ecbe965c8302700040b9
vle28 <vle28@csc.com>
2018-03-06 14:56:32 +0700

    IZUMI-001: Restructure the architect by using jsp.

--- kawaiizumi-web-app/src/main/java/com/izumi/product/controller/DashboardController.java
+++ kawaiizumi-web-app/src/main/java/com/izumi/product/controller/DashboardController.java
@@ -3,15 +3,14 @@
  */
 package com.izumi.product.controller;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Date;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.izumi.product.model.User;
 
 /**
  * @author vle28
@@ -19,31 +18,44 @@ import com.izumi.product.model.User;
  */
 @Controller
 public class DashboardController {
+	@Value("${application.message:Hello World}")
+	private String message = "Hello World";
+
 	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
-	public ModelAndView dashboard() {
-		ModelAndView model = new ModelAndView();
-		model.addObject("users", getUsers());
-		model.setViewName("dashboard");
-		return model;
+	public String dashboard(Map<String, Object> model) {
+		// model.put("users", getUsers());
+		return "index";
 	}
 
-	private List<?> getUsers() {
-		User user = new User();
-		user.setEmail("johndoe123@gmail.com");
-		user.setName("John Doe");
-		user.setAddress("Bangalore, Karnataka");
-		User user1 = new User();
-		user1.setEmail("amitsingh@yahoo.com");
-		user1.setName("Amit Singh");
-		user1.setAddress("Chennai, Tamilnadu");
-		User user2 = new User();
-		user2.setEmail("bipulkumar@gmail.com");
-		user2.setName("Bipul Kumar");
-		user2.setAddress("Bangalore, Karnataka");
-		User user3 = new User();
-		user3.setEmail("prakashranjan@gmail.com");
-		user3.setName("Prakash Ranjan");
-		user3.setAddress("Chennai, Tamilnadu");
-		return Arrays.asList(user, user1, user2, user3);
+	@GetMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("time", new Date());
+		model.put("message", this.message);
+		return "welcome";
 	}
+
+	@RequestMapping("/foo")
+	public String foo(Map<String, Object> model) {
+		throw new RuntimeException("Foo");
+	}
+
+	// private List<?> getUsers() {
+	// User user = new User();
+	// user.setEmail("johndoe123@gmail.com");
+	// user.setName("John Doe");
+	// user.setAddress("Bangalore, Karnataka");
+	// User user1 = new User();
+	// user1.setEmail("amitsingh@yahoo.com");
+	// user1.setName("Amit Singh");
+	// user1.setAddress("Chennai, Tamilnadu");
+	// User user2 = new User();
+	// user2.setEmail("bipulkumar@gmail.com");
+	// user2.setName("Bipul Kumar");
+	// user2.setAddress("Bangalore, Karnataka");
+	// User user3 = new User();
+	// user3.setEmail("prakashranjan@gmail.com");
+	// user3.setName("Prakash Ranjan");
+	// user3.setAddress("Chennai, Tamilnadu");
+	// return Arrays.asList(user, user1, user2, user3);
+	// }
 }
--- kawaiizumi-web-app/src/main/java/com/izumi/product/kawaiizumiwebapp/KawaiizumiWebAppApplication.java
+++ kawaiizumi-web-app/src/main/java/com/izumi/product/kawaiizumiwebapp/KawaiizumiWebAppApplication.java
@@ -7,25 +7,19 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-import com.izumi.product.config.BeanConfig;
-import com.izumi.product.config.WebConfig;
 
 @Configuration
 @ComponentScan(basePackages = "com.izumi")
-@Import({ BeanConfig.class, WebConfig.class })
 @EnableAutoConfiguration
 @SpringBootApplication
 public class KawaiizumiWebAppApplication extends SpringBootServletInitializer {
-	private static Class<?> applicationClass = KawaiizumiWebAppApplication.class;
-
-	public static void main(String[] args) {
-		SpringApplication.run(KawaiizumiWebAppApplication.class, args);
-	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(applicationClass);
+		return application.sources(KawaiizumiWebAppApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KawaiizumiWebAppApplication.class, args);
 	}
 }
