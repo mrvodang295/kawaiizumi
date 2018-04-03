1aec5f7f0c23e75368bc4e73431f709d4b41a1b7
vle28 <vle28@csc.com>
2018-04-03 15:50:37 +0700

    IZUMI-000: Fix bug "strict MIME type checking". (because its MIME type ('application/json') is not a supported stylesheet MIME type, and strict MIME checking is enabled)

--- kawaiizumi-web-app/src/main/resources/static/assets/js/custom.js
+++ kawaiizumi-web-app/src/main/resources/static/assets/js/custom.js
@@ -1,27 +1,10 @@
 
-
-
-/*=============================================================
-    Authour URI: www.binarytheme.com
-    License: Commons Attribution 3.0
-
-    http://creativecommons.org/licenses/by/3.0/
-
-    100% To use For Personal And Commercial Use.
-    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
-   
-    ========================================================  */
-
-
 (function ($) {
     "use strict";
     var mainApp = {
 
         main_fun: function () {
            
-            /*====================================
-              LOAD APPROPRIATE MENU BAR
-           ======================================*/
             $(window).bind("load resize", function () {
                 if ($(this).width() < 768) {
                     $('div.sidebar-collapse').addClass('collapse')
@@ -40,8 +23,6 @@
         }
 
     }
-    // Initializing ///
-
     $(document).ready(function () {
         mainApp.main_fun();
     });
