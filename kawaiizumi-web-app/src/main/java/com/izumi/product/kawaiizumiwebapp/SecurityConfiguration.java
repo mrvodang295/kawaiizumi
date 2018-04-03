9afad110de39da81e4b31b92946291c3b9ee5d78
vle28 <vle28@csc.com>
2018-04-03 11:17:31 +0700

    IZUMI-000: Add minify library, using Thymeleaf Engine for generating view.

---
+++ kawaiizumi-web-app/src/main/java/com/izumi/product/kawaiizumiwebapp/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.izumi.product.kawaiizumiwebapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author vle28
+ *
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+	}
+}
