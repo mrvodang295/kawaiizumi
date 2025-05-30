72539936265c47275e3336450c62c1dbefa3ef27
vle35 <vuhoang.le@dxc.com>
2025-05-30 11:36:24 +0700

    IZUMI-031: Fix error CORS.

---
+++ msg-durian-care/src/main/java/com/kawaiizumi/services/duriancare/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.kawaiizumi.services.duriancare.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	private final CorsProperties corsProperties;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
---
+++ msg-durian-care/src/main/java/com/kawaiizumi/services/duriancare/config/CorsProperties.java
@@ -0,0 +1,20 @@
+package com.kawaiizumi.services.duriancare.config;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+	private List<String> allowedOrigins;
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
